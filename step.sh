58d7328859c5bcfa894f6c6cef665c3970d1d799
Rishabh Karnad <rishabhk@geekyants.com>
2020-05-09 03:14:19 +0530

    Initial commit

---
+++ step.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
+# upload iOS source maps to Sentry
+export SENTRY_PROPERTIES=$SENTRY_PROPERTIES_PATH
+./node_modules/@sentry/cli/bin/sentry-cli react-native appcenter $APPCENTER_APP_ID $REACT_NATIVE_PLATFORM $REACT_NATIVE_BUILD_DIR/CodePush --token $APPCENTER_API_TOKEN $OPTIONS
